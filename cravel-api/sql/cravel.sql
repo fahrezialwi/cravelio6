a5414a8509758fc85c259c4339ecf97004a1042f
fahrezialwi <fahrezialwi@gmail.com>
2019-09-30 01:27:10 +0700

    Connect Reviews component to API

--- cravel-api/sql/cravel.sql
+++ cravel-api/sql/cravel.sql
@@ -18,3 +18,4 @@ select * from trips t
 join pictures p on t.trip_id = p.trip_id
 where t.trip_id = 1;
 select * from pictures where trip_id = 1 and not is_main = 1;
+select review_id, review_title, review_content, star, first_name, last_name from reviews join users on reviews.user_id = users.user_id where trip_id = 1;
--- cravel-app/src/components/trip-detail/Pictures.jsx
+++ cravel-app/src/components/trip-detail/Pictures.jsx
@@ -7,7 +7,7 @@ class Pictures extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            pictures: '',
+            pictures: ''
         }
     }
 
