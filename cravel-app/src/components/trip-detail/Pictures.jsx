b79b97d482a363bb8b31ef4d04af1a60f1a4c3aa
fahrezialwi <fahrezialwi@gmail.com>
2019-10-07 02:18:01 +0700

    Fix jumbotron design

--- cravel-app/src/components/trip-detail/Pictures.jsx
+++ cravel-app/src/components/trip-detail/Pictures.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { URL_API } from '../../helpers'
 import FsLightbox from 'fslightbox-react'
+import '../../styles/pictures.css'
 
 class Pictures extends Component {
 
@@ -45,11 +46,62 @@ class Pictures extends Component {
     render() {
         if(this.state.pictures){
             return (
-                <div className="row mt-3 mb-3">
-                    <div className="col-6 p-0">
-                        <img onClick={() => this.setToogler(!this.state.toogler, 1)} src={this.state.pictures[1]} alt="2" width="100%"/>
-                    </div> 
-                    <div className="col-6">
+              
+                    <div className="main-image">
+                        <div className="favorites">Save</div>
+                        <div onClick={() => this.setToogler(!this.state.toogler, 1)} className="all-images">View All Photos</div>
+                        <div onClick={() => this.setToogler(!this.state.toogler, 1)} className="left-image">
+                            <img 
+                                alt="1" 
+                                src={this.state.pictures[0]} 
+                                className="zooming-image" 
+                                style={{top: "0px"}}
+                                height="425.3333333333333" 
+                                width="638" 
+                            />
+                        </div>
+
+                        <div className="container-middle-image">
+                            <div onClick={() => this.setToogler(!this.state.toogler, 2)} className="middle-image">
+                                <img 
+                                    alt="2" 
+                                    src={this.state.pictures[1]} 
+                                    className="zooming-image"
+                                    style={{top: "0px"}}
+                                    width="318"
+                                />
+                            </div>
+                            <div onClick={() => this.setToogler(!this.state.toogler, 3)} className="middle-image">
+                                <img 
+                                    alt="3" 
+                                    src={this.state.pictures[2]}
+                                    className="zooming-image"
+                                    style={{top: "0px"}}
+                                    width="318"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="container-right-image">
+                            <div onClick={() => this.setToogler(!this.state.toogler, 4)} className="right-image">
+                                <img 
+                                alt="4" 
+                                src={this.state.pictures[3]}
+                                className="zooming-image"
+                                style={{top: "0px"}}
+                                width="318"/>
+                            </div>
+                            <div onClick={() => this.setToogler(!this.state.toogler, 5)} className="middle-image">
+                                <img 
+                                alt="5" 
+                                src={this.state.pictures[4]} 
+                                className="zooming-image"
+                                style={{top: "0px"}}
+                                width="318"/>
+                            </div>
+                        </div>
+                   
+                    {/* <div className="col-6">
                         <div className="row"> 
                             <div className="col-6 p-0">
                                 <img onClick={() => this.setToogler(!this.state.toogler, 2)} src={this.state.pictures[2]} alt="2" width="100%"/>
@@ -64,7 +116,7 @@ class Pictures extends Component {
                                 <img onClick={() => this.setToogler(!this.state.toogler, 5)} src={this.state.pictures[5]} alt="3" width="100%"/>
                             </div>
                         </div> 
-                    </div> 
+                    </div>  */}
 
                     <FsLightbox
                         toggler={this.state.toogler}
